54d0aff47c6e4f7289502dc7c77599ac9feed03d
Jai-kishan <jai@navgurukul.org>
2021-01-20 18:06:07 +0530

    define a fun for history

--- Trivia/forms.py
+++ Trivia/forms.py
@@ -4,7 +4,7 @@ from .models import *
 
 class EnterUserName(forms.Form):
     # ---------comments-----------------------------------------------------#
-    # 
+    #  Useing this from we are getting the User name
     # ------------------------ends here-------------------------------------#
 
     name      = forms.RegexField(label="Name*",     max_length=30,regex=r'^[a-zA-Z ]+$',widget=forms.TextInput(attrs={'class' : 'form-control'}),required=True)
--- Trivia/templatetags/common_tags.py
+++ Trivia/templatetags/common_tags.py
@@ -16,23 +16,33 @@ def get_choices(ques):
 def get_summary(ques_id, summary):
 	ch= summary.config.get(str(ques_id))
 	res = ""
+	ans= []
 	if ch:
 		choice = Choice.objects.filter(id__in=ch)
+		correct_ans = Choice.objects.filter(question__id = ques_id, is_correct=True)
+
+		for cr in correct_ans:
+			ans.append(cr.text)
+
 		for c in range(len(choice)):
 			if len(choice) == 1:
 				res = res + choice[c].text
+				
 			else:
 				if c == (len(choice)-1):
-					res = res + choice[c].text			
+					res = res + choice[c].text
+
 				else:
-					res = res + choice[c].text + "," 
-	return res
+					res = res + choice[c].text + ",  "
+	return res,ans
 
 
 @register.simple_tag
 def get_user_history(res):
-	data=[]
-	for ques_id in res.config:
-		ques = Question.objects.get(id = int(ques_id))
-
+	data= []
+	for ques_id,ch_id in res.config.items():
+		res = Choice.objects.filter(question__id =ques_id,id__in=ch_id)
+		for i in res:
+			data.append(i.question)
+			data.append(i.text)
 	return data
